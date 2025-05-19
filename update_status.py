f880a8419b7e172ba1c6aca38c269fb468bbf2b3
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-05-19 15:48:48 -0400

    separate graphs added

--- update_status.py
+++ update_status.py
@@ -9,3 +9,10 @@ data = np.random.randint(0, 14, len(dates))
 # GitHub Activity like plot (for someone with consistently random work patterns).
 july.heatmap(dates, data, title='Github Activity', cmap="github")
 plt.show()
+
+# Month plot with dates.
+july.month_plot(dates, data, month=5, date_label=True)
+# Month plot with values.
+july.month_plot(dates, data, month=6, value_label=True)
+july.month_plot(dates, data, month=7, value_label=True)
+plt.show()
