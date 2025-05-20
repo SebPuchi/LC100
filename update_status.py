4966a4462db8e367b2fbc30c38bd126f9ea7be72
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-05-19 21:48:30 -0400

    plot lookin pretty damn nice

--- update_status.py
+++ update_status.py
@@ -8,7 +8,7 @@ mpl.rcParams['font.family'] = 'Hack Nerd Font Mono'
 dates = date_range("2025-05-01", "2025-8-31")
 data = np.random.randint(0, 2, len(dates))
 
-fig, ax = plt.subplots(nrows=1, ncols=4)
+fig, ax = plt.subplots(nrows=1, ncols=4, figsize=(12, 3.5))
 
 july.month_plot(dates, data, month=5, ax=ax[0])
 july.month_plot(dates, data, month=6, ax=ax[1])
@@ -16,6 +16,6 @@ july.month_plot(dates, data, month=7, ax=ax[2])
 july.month_plot(dates, data, month=8, ax=ax[3])
 
 fig.suptitle("Completed Days", fontsize=16)
-plt.savefig("./auto_assets/plot.png", dpi=100)
+plt.savefig("./auto_assets/plot.png", dpi=300, bbox_inches='tight')
 plt.show()
 
