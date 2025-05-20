7fb79a39e71e94168f05c479d4963f2d098a7305
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-05-19 21:15:10 -0400

    muli sub plot same axis

--- update_status.py
+++ update_status.py
@@ -3,16 +3,18 @@ import matplotlib.pyplot as plt
 import july
 from july.utils import date_range
 
-dates = date_range("2020-01-01", "2020-12-31")
-data = np.random.randint(0, 14, len(dates))
+dates = date_range("2025-05-01", "2020-8-31")
+data = np.random.randint(0, 2, len(dates))
 
-# GitHub Activity like plot (for someone with consistently random work patterns).
-july.heatmap(dates, data, title='Github Activity', cmap="github")
+
+fig, ax = plt.subplots(nrows=1, ncols=4)
+
+# Plot each month onto the same axis
+july.month_plot(dates, data, month=5,  ax=ax[0])
+july.month_plot(dates, data, month=6,  ax=ax[1])
+july.month_plot(dates, data, month=7,  ax=ax[2])
+july.month_plot(dates, data, month=8,  ax=ax[3])
+
+
 plt.show()
 
-# Month plot with dates.
-july.month_plot(dates, data, month=5, date_label=True)
-# Month plot with values.
-july.month_plot(dates, data, month=6, value_label=True)
-july.month_plot(dates, data, month=7, value_label=True)
-plt.show()
