f39a047a975221ba45f04b4defce8537324e4657
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-05-19 21:41:02 -0400

    save img and asset dir

--- update_status.py
+++ update_status.py
@@ -8,15 +8,14 @@ mpl.rcParams['font.family'] = 'Hack Nerd Font Mono'
 dates = date_range("2025-05-01", "2025-8-31")
 data = np.random.randint(0, 2, len(dates))
 
-
 fig, ax = plt.subplots(nrows=1, ncols=4)
 
-# Plot each month onto the same axis
 july.month_plot(dates, data, month=5, ax=ax[0])
 july.month_plot(dates, data, month=6, ax=ax[1])
 july.month_plot(dates, data, month=7, ax=ax[2])
 july.month_plot(dates, data, month=8, ax=ax[3])
 
 fig.suptitle("Completed Days", fontsize=16)
+plt.savefig("./auto_assets/plot.png", dpi=300, bbox_inches='tight')
 plt.show()
 
