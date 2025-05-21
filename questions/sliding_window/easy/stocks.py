ed0383a72b32a7403606ade6474ac5f42ff86513
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-05-20 21:48:22 -0400

    stock price solution

--- questions/sliding_window/easy/stocks.py
+++ questions/sliding_window/easy/stocks.py
@@ -1,1 +1,20 @@
-test
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+
+        left = 0
+        right = 1
+
+        while right < len(prices):
+
+            if prices[right] < prices[left]:
+                left = right 
+                right = left + 1
+                continue
+
+            if prices[right] - prices[left] > profit:
+                profit = prices[right] - prices[left] 
+            
+            right+=1
+
+        return profit 
