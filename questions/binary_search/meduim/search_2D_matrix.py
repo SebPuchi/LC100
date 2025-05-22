197491c65dde347bb9aa30fb3c1535aae03b3d72
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-05-22 18:39:15 -0400

    search 2D matrix sol

---
+++ questions/binary_search/meduim/search_2D_matrix.py
@@ -0,0 +1,33 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        left = 0
+        right = len(matrix) -1
+
+        while right >= left:
+
+            middle_index = left + ((right - left) // 2)
+
+            if matrix[middle_index][0] == target:
+                return True
+            elif matrix[middle_index][0] > target:
+                right = middle_index -1
+            else: 
+                left = middle_index +1
+        
+        
+        approx_arr = matrix[middle_index]
+
+        left = 0
+        right = len(approx_arr) -1
+        while right >= left:
+
+            middle_index = left + ((right - left) // 2)
+
+            if approx_arr[middle_index] == target:
+                return True
+            elif approx_arr[middle_index] > target:
+                right = middle_index -1
+            else: 
+                left = middle_index +1
+
+        return False
