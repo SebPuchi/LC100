d4a91217b49dce4a1181233b0ffaa853572b17f3
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-05-31 10:57:02 -0500

    fixed solution

--- questions/trees/easy/balanced_binary_tree.py
+++ questions/trees/easy/balanced_binary_tree.py
@@ -8,14 +8,19 @@ class TreeNode:
 class Solution:
     def explore(self, current):
         if current == None:
-            return 0 
+            return (0, True)
 
-        left =  1 + self.explore(current.left)
-        right =  1 + self.explore(current.right)
-        if abs(left - right) == 1 or abs(left-right) == 0:
-            return 0 
-        else: 
-            return 1
+        left_tup =  self.explore(current.left)
+        right_tup =  self.explore(current.right)
+        left, left_bal =  left_tup[0] + 1, left_tup[1]
+        right, right_bal =  right_tup[0] + 1, right_tup[1]
+        is_bal = False 
 
+        if abs(left-right) == 0 or abs(left-right) == 1:
+            print("IN HERE")
+            is_bal = True
+            print("current_bal", is_bal)
+        print(current.val, is_bal, abs(left - right))
+        return (max(left, right), (is_bal and left_bal and right_bal))
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        return self.explore(root) == 1
+        return self.explore(root)[1]
