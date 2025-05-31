946c5d8aa00f4df9dc3b96d899f3ca5258832994
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-05-31 16:37:54 -0500

    same tree sol

---
+++ questions/trees/easy/same_tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+
+class Solution:
+    def explore(self, current, visited): 
+        if current == None: 
+            visited.append(-1) 
+            return 
+        visited.append(current.val) 
+        self.explore(current.left, visited)
+        self.explore(current.right, visited)
+
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        visited_one = []
+        visited_two = []
+
+        self.explore(p, visited_one)
+        self.explore(q, visited_two)
+        return visited_one == visited_two
+        
