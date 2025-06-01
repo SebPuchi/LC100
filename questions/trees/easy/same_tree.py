ec028ef5e1a6b7cfd0f7a70ea0c4db7f6cbce45a
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-06-01 08:35:01 -0500

    better solution for same tree

--- questions/trees/easy/same_tree.py
+++ questions/trees/easy/same_tree.py
@@ -22,3 +22,23 @@ class Solution:
         self.explore(q, visited_two)
         return visited_one == visited_two
         
+
+# Solution 2: More efficient: 
+class Solution:
+    def explore(self, current1, current2): 
+        if current1 == None and current2 == None: 
+            return True
+        elif current1 != None and current2 != None: 
+            matched = False
+    
+            if current1.val == current2.val: 
+                matched = True
+            left_matched = self.explore(current1.left, current2.left)
+            right_matched = self.explore(current1.right, current2.right)
+    
+            return matched and left_matched and right_matched
+        else: 
+            return False
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        return self.explore(p, q)
+        
