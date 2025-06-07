fbcbce876aab8bf73081659efcd4a22f739f16b9
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-06-07 13:51:25 -0500

    updated script

--- update_status.py
+++ update_status.py
@@ -22,6 +22,7 @@ def get_commit_dates(path, since, until):
 
 mpl.rcParams['font.family'] = 'Hack Nerd Font Mono'
 dates = date_range("2025-05-01", "2025-8-31")
+# May need to update last date in the future
 commit_dates = get_commit_dates("./questions/", "2025-05-01", "2025-08-31")
 print("Commit dates found:", commit_dates)
 
@@ -41,25 +42,42 @@ fig.suptitle("Completed Days", fontsize=16)
 # Walk question dir  
 path = "./questions"
 
-questions = {} 
+
+markdown_rows = []
 
 for (root, dirs, files) in os.walk(path):
-    
+    row = ""
     for file in files:
         if file.endswith(".py"):
+            file_path = os.path.join(root, file)
             parts = root.split(os.sep)
-            print(os.path.join(root, file))
             if len(parts) != 4:
                 print("couldn't identify file")
                 continue 
-            
-            # category = parts[2]
-            # difficulty = parts[3] if len(parts) > 3 else "unknown"
-            # question_file = file
-            # question_name = question_file.replace(".py", "").replace("_", " ").title()
+            #adds link thing in markdown
+            row +="| [" +file[:-3] +"](" + file_path +") | " + parts[2] + " | " + parts[3] + " |\n"
 
-            # # Format question display and link
-            # relative_path = os.path.join(root, file).replace("\\", "/")
-            # link = f"[{question_name}]({relative_path})"
-            # row = f"| {link} | True | {category} | `{difficulty}` |"
-            # table_rows.append(row)
+            markdown_rows.append(row)
+
+
+
+# Writing to file
+start_index = None
+
+with open("./README.md", "r") as f:
+    lines = f.readlines()
+
+for i, line in enumerate(lines):
+    if "# TABLE ROW START" in line:
+        start_index = i
+        break
+
+if start_index is not None:
+    new_lines = lines[:start_index + 1] + markdown_rows
+
+    with open("./README.md", "w") as f:
+        f.writelines(new_lines)
+    print("README.md updated successfully.")
+else:
+    print("Marker '# TABLE ROW START' not found in README.md.")
+
