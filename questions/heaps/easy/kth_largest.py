5a034ee92c28bd345f336c2729314166919bdc5c
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-06-08 08:33:01 -0500

    notes and solution updated

--- questions/heaps/easy/kth_largest.py
+++ questions/heaps/easy/kth_largest.py
@@ -5,11 +5,9 @@ class KthLargest:
         self.kth_largest = k
         heapq.heapify(nums)
         self.stream = nums
-        while len(self.stream) > self.kth_largest:
-            heapq.heappop(self.stream)
 
     def add(self, val: int) -> int:
         heapq.heappush(self.stream, val) 
-        if (len(self.stream) > self.kth_largest):
-            heapq.heappop(self.stream) 
+        while len(self.stream) > self.kth_largest:
+            heapq.heappop(self.stream)
         return self.stream[0]
