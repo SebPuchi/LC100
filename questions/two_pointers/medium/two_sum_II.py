8b874ea58f07c41a5793aa7f18c8f7a7f96f0305
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-06-22 14:56:31 -0500

    better two sum II sol

--- questions/two_pointers/medium/two_sum_II.py
+++ questions/two_pointers/medium/two_sum_II.py
@@ -1,4 +1,6 @@
 class Solution(object):
+
+# SOLUTION WITH BINARY SEARCH
     def binarySearch(self, target, nums, left, right):
         while left <= right:
             print(left, right)
@@ -17,3 +19,20 @@ class Solution(object):
             search_index = self.binarySearch(target - numbers[i], numbers, i+1, len(numbers)-1)         
             if (search_index):
                 return [i+1, search_index+1]
+
+
+# BETTER SOLUITION: TWO POINTER
+   def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left = 0
+        right = len(numbers) - 1
+
+        while left < right:
+            if numbers[left] + numbers[right] == target:
+                return [left+1, right+1]
+            elif numbers[left] + numbers[right] < target:
+                left+=1
+            else: 
+                right-=1
+        return []
+
+
