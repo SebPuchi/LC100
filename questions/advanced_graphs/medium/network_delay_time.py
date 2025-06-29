a7722a8de2c918b21d7bfc8944ed86ebe368e903
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-06-28 22:29:14 -0500

    I'm getting very tired of this problem I know how to do it but its so buggy ugh

--- questions/advanced_graphs/medium/network_delay_time.py
+++ questions/advanced_graphs/medium/network_delay_time.py
@@ -36,3 +36,42 @@ class Solution:
                 return -1
         return max(distances.values())
 
+# Another broken attempt :/ 
+import heapq
+
+class Solution:
+
+    def getTime(self, times, source, dest):
+        for i in range(len(times)):
+            if times[i][0] == source and times[i][1] == dest:
+                return times[i][2]
+    def getNeighbors(self, times, node):
+        neighbors = []
+        for i in range(len(times)):
+            if times[i][0] == node:
+                neighbors.append(times[i][1])
+        return neighbors
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        distances = {k: 0}
+        priority_queue = []
+        priority_queue.append((0, k)) 
+        while len(priority_queue) != 0:
+            dist, current = priority_queue.pop()
+            neighbors = self.getNeighbors(times, current)
+            print(neighbors)
+            for neigh in neighbors:
+                weight = self.getTime(times, current, neigh)
+                if neigh in distances:
+                    if distances[current] + weight < distances[neigh]:
+                        distances[neigh] = distances[current] + weight
+                else:
+                    distances[neigh] = distances[current] + weight
+                priority_queue.append((distances[current]+ weight, neigh)) 
+                heapq.heapify(priority_queue)
+        
+        for i in range(1, n+1):
+            print(i)
+            if i not in distances.keys():
+                return -1
+        return max(distances.values())
+
