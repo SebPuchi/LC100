bde59d057af71bb068545dbfa486593cc46fcf0b
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-06-29 22:16:36 -0500

    Finally I finished this dumb network dely question XD

--- questions/advanced_graphs/medium/network_delay_time.py
+++ questions/advanced_graphs/medium/network_delay_time.py
@@ -1,77 +1,39 @@
 import heapq
+import math
 
 class Solution:
-    def findNode(self, times, node):
-        valid_nodes = []
-        for i in range(len(times)):
-            if times[i][0] == node:
-                valid_nodes.append(times[i])
-        
-        return valid_nodes
-
-    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        start_node = self.findNode(times, k)
-        print("START NODE", start_node)
-        distances = {start_node[0]: 0}
-        priority_queue = []
-        priority_queue.append(start_node) 
-        while len(priority_queue) != 0:
-            current = priority_queue.pop()
-            if current:
-                neigbors = self.findNode(times, current[1])
-                for i in range(len(neigbors)):
-                    priority_queue.append(neigbors[i])
-
-                if current[1] in distances:
-                    if current[2] + distances[current[0]] < distances[current[1]]:
-                        distances[current[1]] = current[2] + distances[current[0]]
-                else:
-                    distances[current[1]] = current[2] + distances[current[0]]
-                heapq.heapify(priority_queue)
-                print(distances)
-        
-        for i in range(1, n+1):
-            print(i)
-            if i not in distances.keys():
-                return -1
-        return max(distances.values())
-
-# Another broken attempt :/ 
-import heapq
-
-class Solution:
-
-    def getTime(self, times, source, dest):
-        for i in range(len(times)):
-            if times[i][0] == source and times[i][1] == dest:
-                return times[i][2]
-    def getNeighbors(self, times, node):
+    def getNeighbors( self, times, node):
         neighbors = []
         for i in range(len(times)):
             if times[i][0] == node:
-                neighbors.append(times[i][1])
+                neighbors.append((times[i][2], times[i][1]))
         return neighbors
+
+
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        distances = {k: 0}
-        priority_queue = []
-        priority_queue.append((0, k)) 
-        while len(priority_queue) != 0:
-            dist, current = priority_queue.pop()
-            neighbors = self.getNeighbors(times, current)
-            print(neighbors)
-            for neigh in neighbors:
-                weight = self.getTime(times, current, neigh)
-                if neigh in distances:
-                    if distances[current] + weight < distances[neigh]:
-                        distances[neigh] = distances[current] + weight
-                else:
-                    distances[neigh] = distances[current] + weight
-                priority_queue.append((distances[current]+ weight, neigh)) 
-                heapq.heapify(priority_queue)
-        
-        for i in range(1, n+1):
-            print(i)
-            if i not in distances.keys():
+        adj = {}  
+        for u, v, w in times:
+            if u not in adj:
+                adj[u] = []
+            adj[u].append((v, w))
+
+        distances = {i: math.inf for i in range(1, n+1)}
+        distances[k] = 0
+        min_heap = [(0, k)]
+        while len(min_heap) != 0:
+            curr_dist, u = heapq.heappop(min_heap)
+            if curr_dist > distances[u]:
+                continue
+
+            neighbors = adj[u] if u in adj else []
+
+            for v, w in neighbors:
+                print(v, w)
+                if distances[v] > curr_dist + w:
+                    distances[v] = curr_dist + w
+                    heapq.heappush(min_heap, (distances[v], v))
+
+        for key in distances:
+            if distances[key] == math.inf:
                 return -1
         return max(distances.values())
-
