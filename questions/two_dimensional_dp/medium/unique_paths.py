45fb1597d887551b28c3935c27edbd47291673a6
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-06-30 21:34:08 -0500

    unique paths sol

---
+++ questions/two_dimensional_dp/medium/unique_paths.py
@@ -0,0 +1,21 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        print(m, n)
+        grid = []
+        for i in range(m + 1):
+            row = []
+            for j in range(n + 1):
+                if j == n or i ==m:
+                    row.append(0)
+                else:
+                    row.append(None)
+            grid.append(row)
+        grid[m-1][n-1] = 1
+
+        for i in range(m-1, -1, -1):
+            for j in range(n -1, -1, -1):
+                if i == m -1 and j == n-1:
+                    continue 
+                grid[i][j] = grid[i+1][j] + grid[i][j+1]
+        return grid[0][0]
+   
