570429ef192d2acb417be9509dba0cf713611509
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-07-01 22:01:40 -0500

    incomplete solution for max subarray

--- questions/greedy/medium/maximum_subarray.py
+++ questions/greedy/medium/maximum_subarray.py
@@ -1,2 +1,14 @@
+import math
 class Solution:
-    def maxSubArray(self, nums: List[int]) -> int
+    def maxSubArray(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums) 
+        current_sum = sum(nums[left : right])
+        while left != right:
+            left=+1
+            if sum(nums[left : right]) >= current_sum:
+                current_sum = sum(nums[left : right])
+            right-=1
+            if sum(nums[left : right-1]) >= current_sum:
+                current_sum = sum(nums[left : right-1])
+            print(current_sum)
