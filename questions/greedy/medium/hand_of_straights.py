0d6e302a6d3aa9a1c022161d6ef6c595d4987631
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-07-13 21:05:14 -0500

    hand of straights sol

--- questions/greedy/medium/hand_of_straights.py
+++ questions/greedy/medium/hand_of_straights.py
@@ -11,10 +11,14 @@ class Solution:
         while len(card_hash) != 0:
             smallest_card = min(card_hash.keys())
             card_hash[smallest_card] -=1
-            if card_hash[smalles_card] == 0:
-                del card_hash[smalles_card]
+            if card_hash[smallest_card] == 0:
+                del card_hash[smallest_card]
             
-            for i in range(groupSize -1):
-                
-        print(card_hash)
-
+            for i in range(1, groupSize):
+                if (smallest_card + i) in card_hash:
+                    card_hash[smallest_card + i] -=1
+                    if card_hash[smallest_card + i] == 0:
+                        del card_hash[smallest_card + i]
+                else: 
+                    return False
+        return True
