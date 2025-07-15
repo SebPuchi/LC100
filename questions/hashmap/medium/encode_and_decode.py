c3916a632aa66e3068a1946c85da51e7c3dfaf6e
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-07-14 22:01:31 -0500

    encode / decode attempt

--- questions/hashmap/medium/encode_and_decode.py
+++ questions/hashmap/medium/encode_and_decode.py
@@ -1,5 +1,18 @@
 class Solution:
-
     def encode(self, strs: List[str]) -> str:
+        encoded_string = '' 
+
+        for word in strs:
+            encoded_string+=(str(len(word))+ "$" + word)
+        print(encoded_string)
+        return encoded_string
 
     def decode(self, s: str) -> List[str]:
+        output = []
+        for i in range(len(s)):
+            if s[i].isnumeric() and i+1< len(s) and s[i+1] == "$":
+                print(s[i], s[i+1])
+                output.append(s[i+2 : i + 2 + int(s[i])])
+                i+=(1 + int(s[i]))
+        return output
+
