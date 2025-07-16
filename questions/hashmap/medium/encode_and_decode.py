b2f887d8f436a2532e941dabd8f4dd654cd2ee56
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-07-15 21:30:07 -0500

    encode / decode attmpt 2

--- questions/hashmap/medium/encode_and_decode.py
+++ questions/hashmap/medium/encode_and_decode.py
@@ -1,4 +1,19 @@
 class Solution:
+    def codeFound(self, s, i):
+        num = "" 
+        for i in range(i, len(s)):
+            print("NUM", num)
+            if s[i].isnumeric():
+                num+=s[i]
+                continue
+            elif s[i] == "$":
+                print("NUM", num)
+                return (True, num)
+            return (False, num)
+        return (False, num)
+
+
+
     def encode(self, strs: List[str]) -> str:
         encoded_string = '' 
 
@@ -10,9 +25,11 @@ class Solution:
     def decode(self, s: str) -> List[str]:
         output = []
         for i in range(len(s)):
-            if s[i].isnumeric() and i+1< len(s) and s[i+1] == "$":
-                print(s[i], s[i+1])
-                output.append(s[i+2 : i + 2 + int(s[i])])
-                i+=(1 + int(s[i]))
+            (valid, offset) = self.codeFound(s, i)
+            print(valid, offset)
+            if valid:
+                output.append(s[i+2 : i + 2 + int(offset)])
+                i+=(1 + int(offset))
         return output
+
 
