ce29ff7742d0d1c43787f20184a02835f199b270
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-07-19 23:00:53 -0500

    Outlined solution

--- questions/sliding_window/medium/longest_repeating_character_replacement.py
+++ questions/sliding_window/medium/longest_repeating_character_replacement.py
@@ -1,3 +1,9 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        
+        # Psuedocode
+        # Begin by couting all the freqs of each character within the input s
+        # This can be up to 26
+        # Withing each window, check to see what the current max freq element is
+        # Next check to see if the lenght of the current string allows for k many relpacements 
+        # If so, make the repalcmeents, and update, if not, change the position of the pointers to the next
+
