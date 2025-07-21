747682ef80864dabbe1343e980ada0d48abf6009
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-07-20 21:41:55 -0500

    longest repeating attempt

--- questions/sliding_window/medium/longest_repeating_character_replacement.py
+++ questions/sliding_window/medium/longest_repeating_character_replacement.py
@@ -1,5 +1,23 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        count_hash = {}
+        left, right = 0
+        
+        while left <= right and right < len(s):
+            length = right - left
+
+            current_max = (None, -1)
+            for i in range(left, right + 1):
+                if s[i] in count_hash:
+                    count_hash[s[i]] +=1
+                else:
+                    count_hash[s[i]] = 1
+                if count_hash[s[i]] > current_max[1]:
+                    current_max = (s[i], count_hash[s[i]])
+            if length - current_max[1] <= k
+                # replace
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
         # Psuedocode
         # Begin by couting all the freqs of each character within the input s
         # This can be up to 26
