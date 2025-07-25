6d109a8b83d8314e4401b2985c65c7de8fb35540
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-07-24 22:06:28 -0500

    really close on this one!

--- questions/stack/medium/reverse_polish_notation.py
+++ questions/stack/medium/reverse_polish_notation.py
@@ -1,37 +1,32 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
+
         stack = []
         for i in range(len(tokens)-1, -1, -1):
             stack.append(tokens[i]);
         
-
-        
-        answer = None
         buffer_arr = []
-        while len(stack) =! 0:
+        op_chars = ['+', '-', '*', '/']
+        while not len(stack) == 0:
+            print("stack", stack)
             current = stack.pop()
-            if isinstance(current, int):
-                buffer_arr.append(current)
+            print("CURRENT", current)
+            if current not in op_chars:
+                buffer_arr.append(int(current))
+                print(buffer_arr)
             else:
                 match current:
                     case "+":
-                        if answer = None:
-                            answer = sum(buffer_arr)
-                        else:
-                            answer += sum(buffer_arr)
+                        stack.append(sum(buffer_arr))
                     case "-":
-                        remainder = buffer_arr[0]
-                        for i in range(1, len(buffer_arr)-1):
-                            remainder-= buffar_ar[i]
-                        if answer = None:
-                            answer = remainder
-                        else:
-                            answer -= remainder
+                         stack.append(buffer_arr[0] - buffer_arr[1])
                     case "*":
-                        remainder = buffer_arr[0]
-                        for i in range(1, len(buffer_arr)-1):
-                            remainder-= buffar_ar[i]
-                        if answer = None:
-                            answer = remainder
-                buffer_sum = []
- 
+                         stack.append(buffer_arr[0] * buffer_arr[1])
+                    case "/":
+                         stack.append(buffer_arr[0] / buffer_arr[1])
+                buffer_arr = []
+                if len(stack) ==1:
+                    break
+                        
+        return stack[0]
+
