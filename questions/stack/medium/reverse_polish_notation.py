f80afad3a8bc2cba9cba81feae679b4f9025ccbb
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-07-26 22:54:00 -0500

    reverse polish solution

--- questions/stack/medium/reverse_polish_notation.py
+++ questions/stack/medium/reverse_polish_notation.py
@@ -1,22 +1,22 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-
-        stack = []
-        for i in range(len(tokens)-1, -1, -1):
-            stack.append(tokens[i]);
-        
-        buffer_arr = []
         op_chars = ['+', '-', '*', '/']
-        while not len(stack) == 0:
-            print("stack", stack)
-            current = stack.pop()
-            print("CURRENT", current)
-            # need to reconsider if case switiching is correct here / adding to stack in reverse order
-            if current not in op_chars:
-                buffer_arr.append(int(current))
-                print(buffer_arr)
-                if len(stack) ==1:
-                    break
-                        
-        return stack[0]
+        stack = []
+        for i in range(len(tokens)):
+            if tokens[i] in op_chars:
+                second = int(stack.pop())
+                first = int(stack.pop())
+                match tokens[i]:
+                    case "+":
+                        stack.append(first + second)
+                    case "-":
+                         stack.append(first - second)
+                    case "*":
+                         stack.append(first * second)
+                    case "/":
+                         stack.append(first / second)
+            else: 
+                stack.append(tokens[i])
+        print(stack)
+        return int(stack[0])
 
