9dcc500cff472bcc9ed5a01611f53ceb42d0c901
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-07-31 22:10:19 -0500

    attempt #2

--- questions/heaps/medium/kth_largest_element_in_array.py
+++ questions/heaps/medium/kth_largest_element_in_array.py
@@ -1,12 +1,29 @@
 import heapq
 class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
+    def sort(self, nums, k):
+        int_max = nums[0]
+        index = 0
+        for z in range(k):
+            for i in range(len(nums) - z):
+                print(nums[i])
+                if nums[i] > int_max:
+                    int_max = nums[i]
+                    index = i
+            temp = nums[len(nums)-1-z]
+            nums[len(nums) - 1 -z] = int_max 
+            nums[index] = temp
 
-       for i in range(len(nums)):
-            nums[i] =nums[i] * -1
+            print(int_max, nums)
+        return nums[len(nums)-k]
+
+
+
         
-       print(nums)
-       for i in range(len(nums)):
-            val = heapq.heappop(nums) 
-            print(val)
-       print(nums
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        no_dup = []
+        for i in range(len(nums)):
+            if nums[i] not in no_dup:
+                no_dup.append(nums[i])
+ 
+        return(self.sort(no_dup, k))
+
