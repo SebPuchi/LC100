b078367d6fb17264c9273ffffa19e66f4eaf8a6e
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-08-04 06:54:15 -0500

    clone graph solution

--- questions/graphs/medium/clone_graph.py
+++ questions/graphs/medium/clone_graph.py
@@ -6,39 +6,31 @@ class Node:
         self.neighbors = neighbors if neighbors is not None else []
 """
 
-# Definition for a Node.
-class Node:
-    def __init__(self, val = 0, neighbors = None):
-        self.val = val
-        self.neighbors = neighbors if neighbors is not None else []
-"""
-
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
 
         if node:
+            new_nodes = {}
+            
             visited = []
             stack = []
             stack.append(node)
-            start_node = None 
-            count = 0
             while len(stack) !=0:
-                new_node = Node()
                 current = stack.pop(0)
                 # Mark as visited 
-                visited.append(current.val)
-                new_node.val = current.val
-                temp_adj_list = []
+                visited.append(current)
+                if current.val not in new_nodes:
+                    new_nodes[current.val] = Node(current.val)
                 for neighbor in current.neighbors:
-                    if neighbor.val not in visited:
-                        print("NODE", current.val, "NEIGHBOR", neighbor.val)
+                    if neighbor not in visited:
                         stack.append(neighbor)
-                    temp_adj_list.append(neighbor)
-                new_node.neighbors = temp_adj_list
-                if count == 0:
-                    start_node = new_node
-                count+=1
-                print(stack)
-            return start_node
+                    if neighbor.val not in new_nodes:
+                        new_nodes[neighbor.val] = Node(neighbor.val)
+                    neighbor_list = new_nodes[current.val].neighbors
+                    print("NE", neighbor.val)
+                    if neighbor not in neighbor_list:
+                        neighbor_list.append(new_nodes[neighbor.val])
+            return new_nodes[1]
         else:
-            return node cloneGraph(self, node: Optional['Node']) -> Optional['Node']
+            return node
+
