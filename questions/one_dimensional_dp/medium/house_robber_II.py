ec9eda745862b3717287b0849a29b7b1143cbcc1
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-08-05 19:21:41 -0500

    house robber solution

--- questions/one_dimensional_dp/medium/house_robber_II.py
+++ questions/one_dimensional_dp/medium/house_robber_II.py
@@ -1,16 +1,18 @@
 class Solution:
 
     def recurseTwo(self, i, nums, memo):
-        if i == 2:
-            memo[i] = nums[2] 
-        elif i == 3:
-            memo[i] = max(nums[2], nums[3])
+        if i == 1:
+            memo[i] = nums[1] 
+        elif i == 2:
+            memo[i] = max(nums[1], nums[2])
         else:
             memo[i] = max(memo[i-2] + nums[i], memo[i-1])
  
     def recurseOne(self, i, nums, memo):
-        if i == 0 or i == 1:
-            memo[i] = max(nums[i-2] + nums[i], nums[i-1])
+        if i == 0:
+            memo[i] = nums[0] 
+        elif i == 1:
+            memo[i] = max(nums[0], nums[1])
         else:
             memo[i] = max(memo[i-2] + nums[i], memo[i-1])
     
@@ -22,10 +24,13 @@ class Solution:
         else:
             memo_one = {}
             memo_two = {}
-        for i in range(len(nums)-2):
+        for i in range(len(nums)-1):
             self.recurseOne(i, nums, memo_one)
-        for i in range(2, len(nums)):
+        for i in range(1, len(nums)):
             self.recurseTwo(i, nums, memo_two)
         vals_one = list(memo_one.values())
+        print(vals_one)
         vals_two = list(memo_two.values())
+        print(vals_two)
         return max(max(vals_one), max(vals_two))
+
