9f409e8320fe874d5988c9becc652cc44b1bdb9d
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-08-07 21:44:01 -0500

    max depth stack frame

--- questions/trees/easy/max_depth_binary_tree.py
+++ questions/trees/easy/max_depth_binary_tree.py
@@ -3,7 +3,7 @@ class Solution:
         if current:
             depth+=1
             left = self.explore(current.left, depth)
-            right =self.explore(current.right, depth)
+            right = self.explore(current.right, depth)
             return max(left, right)
         else:
             return depth
@@ -11,4 +11,21 @@ class Solution:
     
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         return self.explore(root, 0)     
-        
+
+# Max depth iterative
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root:
+            stack = []
+            stack.append((root, 0))
+            current_max = -1
+            while len(stack) != 0:
+                current = stack.pop()
+                node, depth = current
+                if node:
+                    stack.append((node.left, depth+1))
+                    stack.append((node.right, depth+1))
+                elif depth > current_max:
+                    current_max = depth
+            return current_max
+        return 
