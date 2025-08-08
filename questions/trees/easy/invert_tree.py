0e29ba8a86df23dd8f3f774d89accee8fc3262ac
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-08-07 21:32:16 -0500

    iterative solution to invert binary tree

--- questions/trees/easy/invert_tree.py
+++ questions/trees/easy/invert_tree.py
@@ -28,3 +28,20 @@ class Solution:
         self.explore(root, visited_nodes)
         return root
 
+# Iterative solution:
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+
+        if root:
+            stack = []
+            stack.append(root)
+            while len(stack) !=0:
+                current = stack.pop()
+                if current:
+                    temp = current.left
+                    current.left = current.right
+                    current.right = temp
+                    stack.append(current.left)
+                    stack.append(current.right)
+        
+        return roo
