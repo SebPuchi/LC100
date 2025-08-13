79688a7023e105a724ced8aeb98954318c864803
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-08-13 06:58:09 -0500

    longest repating string relplacment

--- questions/sliding_window/medium/longest_repeating_character_replacement.py
+++ questions/sliding_window/medium/longest_repeating_character_replacement.py
@@ -1,21 +1,19 @@
-# Cleaned up version 
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         count = {}
-        max_count = 0
-        left = 0
         result = 0
-
-        # can now think about solution that cuts back time with max freq trick
-        for right in range(len(s)):
-            count[s[right]] = count.get(s[right], 0) + 1
-            max_count = max(max_count, count[s[right]])
-
-            while (right - left + 1) - max_count > k:
-                count[s[left]] -= 1
-                left += 1
-
-            result = max(result, right - left + 1)
-
-        return result
+        left = 0
+        right = 0
+        while right < len(s):
+            count[s[right]] = 1 + count.get(s[right], 0)
+            if ((right-left) +1) - max(count.values()) <= k:
+                result = max(result, (right-left) + 1)
+                right+=1
+            else:
+                count[s[left]]-=1
+                left+=1
+                right+=1
+            print(count)
+            print(result)
+        return(result)
 
