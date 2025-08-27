5fdf09bbba3ef4df2b09c6f81e2834d064a3422a
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-08-27 07:27:13 -0400

    group anagrams resolve / notes on self squared

--- questions/hashmap/medium/group_anagrams.py
+++ questions/hashmap/medium/group_anagrams.py
@@ -1,4 +1,24 @@
 class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hash_map = {}
+
+        for i in range(len(strs)):
+            current_word = strs[i]
+            key = [0] * 26
+
+            current_word.lower()
+            for letter in current_word:
+                key[ord(letter) -97] +=1
+            key = tuple(key)
+            if key in hash_map.keys():
+                hash_map[key].append(current_word)
+            else:
+                hash_map[key] = [current_word]
+        return list(hash_map.values()
+
+
+
+class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]
         # Need to consider how to make an array a key, as keys need to be immutable, and arrays are not
         hashmap = {}
