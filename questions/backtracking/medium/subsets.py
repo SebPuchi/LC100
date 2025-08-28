8f71c417569ce97c9135c84149ac0b64eac3389a
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-08-28 13:41:37 -0400

    subsets better solution

--- questions/backtracking/medium/subsets.py
+++ questions/backtracking/medium/subsets.py
@@ -1,5 +1,22 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+
+        def backtrack(index, current_path):
+            if index >= len(nums):
+                result.append(current_path.copy())
+                return 
+            current_path.append(nums[index])
+            backtrack(index+1, current_path)
+            current_path.pop()
+            backtrack(index+1, current_path)
+
+        backtrack(0, [])
+        return(result)
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
         
         def backtrack(start: int, path: List[int]):
