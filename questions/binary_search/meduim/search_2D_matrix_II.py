dac31f32084e526b5c06197b7eec889817d88da0
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-09-01 10:24:14 -0400

    Linear solution to search 2D matrix II

--- questions/binary_search/meduim/search_2D_matrix_II.py
+++ questions/binary_search/meduim/search_2D_matrix_II.py
@@ -1,3 +1,23 @@
+# Linear soltuion:
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        i = len(matrix) -1
+        j = 0
+
+        while i >=0 and j < len(matrix[0]):
+            print(matrix[i][j])
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] < target:
+                j+=1
+            else:
+                i-=1
+        return False
+        
+
+
+
+# Binary Search Solution
 class Solution:
     def binarySearch(self, arr, row, target):
         left = 0
