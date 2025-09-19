d437f20a412683d71de5390177b145f494efebce
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-09-19 17:17:48 -0400

    merge k sorted lists better solution

--- questions/linked_list/hard/merge_k_sorted_linked_lists.py
+++ questions/linked_list/hard/merge_k_sorted_linked_lists.py
@@ -3,36 +3,21 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
-import math
-class Solution:    
-    def findValid (self, lists):
-        valid = [False] * len(lists)
-        for i in range(len(valid)):
-            node = lists[i]
-            if node != None:
-                valid[i] = True
-        
-        return valid
-
+class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         dummyNode = ListNode()
-        valid_start = self.findValid(lists)
+        heap = []
 
-        current_node = dummyNode
-        while True in valid_start:
-            current_min = math.inf
-            min_index = None
-            for i in range(len(valid_start)):
-                if valid_start[i] and lists[i].val < current_min:
-                    current_min = lists[i].val
-                    min_index = i
-            lists[min_index] = lists[min_index].next
-            updated_node = ListNode(current_min)
-            current_node.next = updated_node
-            current_node = updated_node
-            if lists[min_index] == None:
-                valid_start[min_index] = False
-        
+        for i, node in enumerate(lists):
+            if node != None:
+                heap.append((node.val, i, node))
+        heapq.heapify(heap)
+
+        current = dummyNode
+        while heap:
+            min_val, i,  node = heapq.heappop(heap)
+            current.next = node
+            current = current.next
+            if node.next != None:
+                heapq.heappush(heap, (node.next.val, i, node.next))
         return dummyNode.next
-
